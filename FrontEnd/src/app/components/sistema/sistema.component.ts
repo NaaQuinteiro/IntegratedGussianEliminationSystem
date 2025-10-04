220941aa81d502f15b14d5d765829a1067c63d9e
NaaQuinteiro <thaynaquinteiro@gmail.com>
2025-10-04 15:34:07 -0300

    Feat: ajustes de estilo e layout

--- FrontEnd/src/app/components/sistema/sistema.component.ts
+++ FrontEnd/src/app/components/sistema/sistema.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { SistemaService, SistemaRequest, SistemaResponse } from '../../service/sistema.service';
 import { FormGroup, FormControl, FormArray, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import confetti from 'canvas-confetti';
 
 @Component({
   selector: 'app-sistema',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
-  templateUrl: './sistema.component.html'
+  templateUrl: './sistema.component.html',
+  styleUrl: './sistema.component.scss'
 })
 export class SistemaComponent {
   resultado: SistemaResponse | null = null;
   tamanho: number = 2;
   form: FormGroup  = new FormGroup({});
+
+  @ViewChild ('resultadoContainer') resultadoRef!: ElementRef;
 
   constructor(private sistemaService: SistemaService) {
     this.iniciarForm(this.tamanho);
@@ -39,6 +43,10 @@ export class SistemaComponent {
     return this.form.get('b') as FormArray;
   }
 
+  getB(i: number): FormControl {
+    return this.vetorB.at(i) as FormControl;
+  }
+
   getLinhaA(i: number): FormArray {
     return this.matrizA.at(i) as FormArray;
   }
@@ -63,6 +71,55 @@ export class SistemaComponent {
 
     this.sistemaService.resolverSistema(payload).subscribe(res => {
       this.resultado = res;
+
+      // Dispara confete baseado na classificação
+      if (res.classificacao) {
+        this.dispararConfete(res.classificacao);
+      }
+
+      // Rola até o resultado
+      setTimeout(() => this.rolarParaResultado(), 100);
+    
     });
+  }
+
+  trackByIndex(index: number, item: any) {
+    return index;
+  }
+
+  dispararConfete(classificacao: string) {
+    switch (classificacao) {
+      case 'SPD':
+        confetti({
+          particleCount: 100,
+          spread: 70,
+          origin: { y: 0.6 },
+          colors: ['#00ff00', '#00cc00', '#009900'] // verde
+        });
+        break;
+      case 'SPI':
+        confetti({
+          particleCount: 150,
+          spread: 120,
+          origin: { y: 0.6 },
+          shapes: ['star', 'circle'],
+          colors: ['#ff9900', '#ffcc00', '#ffaa00'] // laranja/dourado
+        });
+        break;
+      case 'SI':
+        confetti({
+          particleCount: 80,
+          spread: 50,
+          origin: { y: 0.6 },
+          colors: ['#ff0000', '#cc0000'], // vermelho
+        });
+        break;
+    }
+  }
+
+  rolarParaResultado() {
+    if (this.resultadoRef) {
+      this.resultadoRef.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   }
 }
